681c26ad9aea2b31cb0a4a052ffb858fcb35049e
Thomas Fränkler <thomas.fraenkler@draeger.com>
2015-09-24 09:59:39 +0200

    add test text

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,11 +6,12 @@ import world.World;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
+
+
 		Plan plan = new Plan(World.grid, World.start, World.goal);
 		System.out.println(plan);
-		
+		System.out.println("Test");
+
 
 	}
 
