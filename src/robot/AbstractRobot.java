1f79fc18d428c6d65c0240869457b0ec4fffd661
Thomas Fränkler <thomas.fraenkler@draeger.com>
2015-09-24 16:30:57 +0200

    add simulation need to implement estimation

--- src/robot/AbstractRobot.java
+++ src/robot/AbstractRobot.java
@@ -1,7 +1,10 @@
 package robot;
 
+import java.util.Random;
+
 public class AbstractRobot {
 
+	private final double max_turning_angle = Math.PI;
 
 	protected double x;
 	protected double y;
@@ -34,6 +37,53 @@ public class AbstractRobot {
 		this.distance_noise = distance_noise;
 		this.measurement_noise = measurement_noise;
 		this.turning_noise = turning_noise;
+	}
+
+	public void move(double turning , double distance){
+
+		turning = new Random().nextGaussian() * measurement_noise + turning;
+		distance = new Random().nextGaussian() * measurement_noise + distance;
+
+		//physical limitations
+		turning = Math.max(-max_turning_angle, turning);
+		turning = Math.min(max_turning_angle, turning);
+		distance = Math.max(0.0, distance);
+
+		// motion
+		heading += turning;
+		heading = angle_trunc(heading);
+		x += distance * Math.cos(heading);
+		y += distance * Math.sin(heading);
+
+	}
+
+	public static double calculateDistance(double[] point1 , double[] point2){
+		return Math.sqrt(((point2[0] - point1[0])*(point2[0] - point1[0]) + ((point2[1] - point1[1])*(point2[1] - point1[1]))));
+	}
+
+	private double angle_trunc(double angle){
+		while (angle < 0.0) {
+			angle += Math.PI * 2;
+		}
+		return ((angle+Math.PI) % (Math.PI * 2 )) - Math.PI;
+	}
+
+	public void sense(){
+		// add gaussian measurement noise to location
+		x = new Random().nextGaussian() * measurement_noise + x;
+		y = new Random().nextGaussian() * measurement_noise + y;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
 	}
 
 }
--- src/simulations/RunAwaySimulation.java
+++ src/simulations/RunAwaySimulation.java
@@ -1,5 +1,6 @@
 package simulations;
 
+import filter.KalmanFilter;
 import robot.AbstractRobot;
 import robot.RunAwayRobot;
 
@@ -10,7 +11,41 @@ public class RunAwaySimulation {
 		AbstractRobot testRobot = new RunAwayRobot(2.1, 4.3, 0.5, 2 * Math.PI, 1.5);
 		testRobot.setNoise(0.0, 0.0, 0.0);
 
-		
+		boolean localized = false;
+		int counter = 0;
+		double distance_tolerance = 0.01 * testRobot.getDistance();
+
+		while (true) {
+
+			testRobot.sense();
+			double[] nextPos = KalmanFilter.estimate_next_position(testRobot);
+			((RunAwayRobot) testRobot).move_in_Circle();
+
+			double error = AbstractRobot.calculateDistance(nextPos, new double[]{testRobot.getX() , testRobot.getY()});
+
+			System.out.println(" distance -> "+ error);
+
+			if (error <= distance_tolerance) {
+				localized = true;
+			}
+
+			if (localized) {
+				System.out.println("Yeah -> Steps: " + counter);
+				break;
+			}
+
+			if (counter > 10 ) {
+				System.out.println("Sorry!");
+				break;
+			}
+
+
+
+
+
+			counter++;
+
+		}
 
 
 	}
